1a9da1ae99578f192f98a37cf3d4e0428d76b4f3
Luíz Henrique de Carvalho Teles <rlyowlly19@gmail.com>
2022-02-01 13:00:03 -0300

    tipos booleanos

---
+++ js/TruthyFalsy.js
@@ -0,0 +1,19 @@
+//boolean
+const usuarioLogado = true
+const contaPaga = false;
+
+//Truthy e falsy
+/*
+0 => false
+1 =>true
+
+*/
+
+console.log(0 == false)
+console.log("" == false)
+console.log(1 == false)
+
+let minhaVar;
+let VarNull= null;
+
+console.log(VarNull, minhaVar)
