90f0d9c2cb367bdb18af71c8530e5de6dc9778ce
Luíz Henrique de Carvalho Teles <rlyowlly19@gmail.com>
2022-02-01 13:46:59 -0300

    funções

---
+++ js/aula01/funcoes.js
@@ -0,0 +1,17 @@
+/*
+let x = "";
+console.log(x)
+x = "oi"
+*/
+
+function ImprimeText(texto) { 
+    console.log(texto);
+}
+
+function Soma() {
+    return resultado = 2 + 2;
+}
+
+ImprimeText("oi");
+console.log(Soma());
+ImprimeText(Soma());
