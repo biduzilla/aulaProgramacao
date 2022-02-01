203d23932ce4be4083a0271a758d677d88e96c3a
Luíz Henrique de Carvalho Teles <rlyowlly19@gmail.com>
2022-02-01 11:40:10 -0300

    aula 01

--- aula01/boolean.js
+++
@@ -1,6 +0,0 @@
-const primeiroNum = 5;
-const segunNum = 6;
-
-console.log(primeiroNum === segunNum);
-
-const teste = "teste";
--- aula01/tipoString.js
+++
@@ -1,9 +0,0 @@
-
-
-const text = "olá mundo";
-const text1 = "toddy";
-const text2 = 'toddy';
-
-console.log(text, text1, text2);
-
-const tests = "teste";
