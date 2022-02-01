71741f0dfa58bf675495360724c68451a4452d87
Luíz Henrique de Carvalho Teles <rlyowlly19@gmail.com>
2022-02-01 12:47:48 -0300

    aula 02

---
+++ TipoVariavel.js
@@ -0,0 +1,31 @@
+//var altura = 5;
+//var comprimento = 7;
+
+//area = altura * comprimento;
+
+//let forma = 'quadrado';
+//let altura = 5;
+//let comprimento = 7;
+//let area;
+
+/*if(forma === 'quadrado') {
+    area = altura * comprimento;
+} else {
+    area = (altura * comprimento)/2;
+}
+*/
+
+const formas = 'quadrado';
+const alturas = 5;
+const comprimentos = 7;
+let areas;
+
+if (forma === 'quadrado') {
+    area = altura * comprimento;
+} else {
+    area = (altura * comprimento) / 2;
+}
+
+
+
+console.log(area);
